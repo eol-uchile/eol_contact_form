e039d3dfeafaa12c8e32a27728712fb50e32200d
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-12-17 16:39:42 -0300

    chore: update semantic version to 0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="eol_contact_form",
-    version="0.5.2",
+    version="0.5.3",
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
     description="Eol Contact Form",
